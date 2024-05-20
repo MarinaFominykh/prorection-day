f1c3e7cb68f125aafad338eda45b7f5d0835dbd0
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2024-05-20 14:38:06 +0300

    секция об акции

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,52 @@
-console.log("123");
+import {
+  burgerButton,
+  closeBurgerButton,
+  menuMobile,
+  sharedForm,
+  KEY,
+} from "../utils/constans.js";
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+
+//   const formData = new FormData(e.target);
+//   const formJson = Object.fromEntries(formData.entries());
+//   const { name, email, company, sum, other } = formJson;
+//   const otherSum = addTwoZeros(sum || other);
+//   const id = e.target.id;
+//   let options = {
+//     widget_key: KEY,
+//     amount: otherSum,
+//     merchant_fields: {
+//       name,
+//       email,
+//       company,
+//     },
+
+//     user_fundraising_program_id: id,
+//     payment_scheme: "single",
+//     user_email: email,
+//   };
+//   console.log("options", options);
+
+//   let M = new Mixplat(options);
+//   M.build();
+//   M.setSuccessCallback(function (o, i) {
+//     console.log("success");
+//   });
+// };
+
+burgerButton.addEventListener("click", () => {
+  menuMobile.classList.add("header__menu-mob_visible");
+});
+closeBurgerButton.addEventListener("click", () => {
+  menuMobile.classList.remove("header__menu-mob_visible");
+});
+
+// sharedForm.addEventListener("submit", handleSubmit);
+// Приведение суммы к минорным единицам:
+function addTwoZeros(string) {
+  let number = parseInt(string);
+  number = number * 100;
+  return number;
+}
---
+++ utils/constans.js
@@ -0,0 +1,7 @@
+const burgerButton = document.querySelector(".header__burger");
+const closeBurgerButton = document.querySelector(".header__menu-close");
+const menuMobile = document.querySelector(".header__menu-mob");
+const sharedForm = document.forms.shared;
+const KEY = "d78fe53b-ee4a-4ab4-8273-e5c4d08226f3";
+
+export { burgerButton, closeBurgerButton, menuMobile, sharedForm, KEY };
