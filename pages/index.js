2620ef90ff9a618858a53112544ec489605b42f4
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2024-05-22 10:04:22 +0300

    адаптив для секции с детьми

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import {
   burgerButton,
   closeBurgerButton,
   menuMobile,
-  sharedForm,
-  KEY,
   mainPopup,
   mainPopupOpen,
   darknessPopup,
@@ -14,76 +12,80 @@ import {
   betrayalPopupOpen,
   lonelinessPopup,
   lonelinessPopupOpen,
-} from '../utils/constans.js';
+  childrenButtons,
+} from "../utils/constans.js";
 
 const openPopup = (popup) => {
-  popup.classList.add('popup_opened');
+  popup.classList.add("popup_opened");
 };
 const closePopup = (e) => {
-  if (!e.target.classList.contains('mixplat__form')) {
-    e.target.classList.remove('popup_opened');
+  if (!e.target.classList.contains("mixplat__form")) {
+    e.target.classList.remove("popup_opened");
   }
 };
 
-burgerButton.addEventListener('click', () => {
-  menuMobile.classList.add('header__menu-mob_visible');
+burgerButton.addEventListener("click", () => {
+  menuMobile.classList.add("header__menu-mob_visible");
 });
-closeBurgerButton.addEventListener('click', () => {
-  menuMobile.classList.remove('header__menu-mob_visible');
+closeBurgerButton.addEventListener("click", () => {
+  menuMobile.classList.remove("header__menu-mob_visible");
 });
 
-mainPopupOpen.addEventListener('click', () => {
+mainPopupOpen.addEventListener("click", () => {
   openPopup(mainPopup);
 });
+childrenButtons.forEach((button) => {
+  button.addEventListener("click", () => openPopup(mainPopup));
+});
 
-mainPopup.addEventListener('click', (e) => {
+mainPopup.addEventListener("click", (e) => {
   closePopup(e);
 });
-darknessPopupOpen.addEventListener('click', () => {
+darknessPopupOpen.addEventListener("click", () => {
   openPopup(darknessPopup);
 });
 
-darknessPopup.addEventListener('click', (e) => {
+darknessPopup.addEventListener("click", (e) => {
   closePopup(e);
 });
-violencePopupOpen.addEventListener('click', () => {
+violencePopupOpen.addEventListener("click", () => {
   openPopup(violencePopup);
 });
 
-violencePopup.addEventListener('click', (e) => {
+violencePopup.addEventListener("click", (e) => {
   closePopup(e);
 });
 
-betrayalPopupOpen.addEventListener('click', () => {
+betrayalPopupOpen.addEventListener("click", () => {
   openPopup(betrayalPopup);
 });
 
-betrayalPopup.addEventListener('click', (e) => {
+betrayalPopup.addEventListener("click", (e) => {
   closePopup(e);
 });
 
-lonelinessPopupOpen.addEventListener('click', () => {
+lonelinessPopupOpen.addEventListener("click", () => {
   openPopup(lonelinessPopup);
 });
 
-lonelinessPopup.addEventListener('click', (e) => {
+lonelinessPopup.addEventListener("click", (e) => {
   closePopup(e);
 });
 
-const fearsSlider = new Swiper('.fears__swiper', {
+const fearsSlider = new Swiper(".fears__swiper", {
   slidesPerView: 1,
   spaceBetween: 12,
   pagination: {
-    el: '.fears__pagination',
-    type: 'fraction',
+    el: ".fears__pagination",
+    type: "fraction",
     renderFraction: function (currentClass) {
       return '<span class="' + currentClass + '"></span>';
     },
   },
 
   navigation: {
-    nextEl: '.fears__next',
-    prevEl: '.fears__prev',
+    nextEl: ".fears__next",
+    prevEl: ".fears__prev",
   },
   speed: 800,
   breakpoints: {
@@ -94,22 +96,24 @@ const fearsSlider = new Swiper('.fears__swiper', {
   },
 });
 
-const childrenSlider = new Swiper('.children__swiper', {
+const childrenSlider = new Swiper(".children__swiper", {
   slidesPerView: 1,
-  spaceBetween: 40,
+  spaceBetween: 24,
+  autoHeight: true,
   speed: 800,
   pagination: {
-    el: '.children__pagination',
+    el: ".children__pagination",
   },
 
   navigation: {
-    nextEl: '.children__next',
-    prevEl: '.children__prev',
+    nextEl: ".children__next",
+    prevEl: ".children__prev",
   },
   breakpoints: {
     1080: {
       slidesPerView: 2.15,
       spaceBetween: 40,
+      autoHeight: false,
     },
   },
 });
--- utils/constans.js
+++ utils/constans.js
@@ -17,15 +17,12 @@ const betrayalPopupOpen = document.querySelector(".betrayal__button");
 const lonelinessPopup = document.querySelector(".loneliness__popup");
 const lonelinessPopupOpen = document.querySelector(".loneliness__button");
 
-const sharedForm = document.forms.shared;
-const KEY = "d78fe53b-ee4a-4ab4-8273-e5c4d08226f3";
+const childrenButtons = document.querySelectorAll(".children__button");
 
 export {
   burgerButton,
   closeBurgerButton,
   menuMobile,
-  sharedForm,
-  KEY,
   mainPopup,
   mainPopupOpen,
   darknessPopup,
@@ -36,4 +33,5 @@ export {
   betrayalPopupOpen,
   lonelinessPopup,
   lonelinessPopupOpen,
+  childrenButtons,
 };
